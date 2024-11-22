1a5ba86fabbd848f45fe1ec28776bdfcd74ecbe7
Adil Bel Mqadem <abel-mqa@e1r1p9.1337.ma>
2024-11-22 23:00:40 +0100

    ex00 and ex01 done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,5 +1,20 @@
 #include "iter.hpp"
 
+void f(int const &i) {
+    std::cout << "Value: " << i << std::endl;
+}
+
+void fo(char const &c) {
+    std::cout << "Value: " << c << std::endl;
+}
+
 int main() {
-    
+    int tab[] = {23, 10, 42, 13, 37, 99};
+    ::iter(tab, sizeof(tab) / sizeof(tab[0]), f);
+
+    std::endl(std::cout);
+
+    char tab2[] = {'a', 'b', 'c'};
+    ::iter(tab2, sizeof(tab2) / sizeof(tab2[0]), fo);
+    return 0;
 }
