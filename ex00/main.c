fbdbdb3a69f339157b636f6366e7d11d21bca2c7
Adil Bel Mqadem <abel-mqa@e1r1p9.1337.ma>
2024-11-22 18:50:30 +0100

    ex00

--- ex00/main.c
+++ ex00/main.c
@@ -1,5 +1,28 @@
 #include "whatever.hpp"
 
 int main() {
-    
+    int a = 2;
+    int b = 3;
+    std;;cout << "Before swap: a = " << a << ", b = " << b << std::endl;
+    ::swap(a, b);
+    std::cout << "After swap: a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    std::cout << "Before swap: c = " << c << ", d = " << d << std::endl;
+    ::swap(c, d);
+    std::cout << "After swap: c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+    float e = 42.42;
+    float f = 21.21;
+    std::cout << "Before swap: e = " << e << ", f = " << f << std::endl;
+    ::swap(e, f);
+    std::cout << "After swap: e = " << e << ", f = " << f << std::endl;
+    std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+    std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+    return 0;
 }
