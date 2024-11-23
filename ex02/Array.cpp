cf755ef7a213db376c0e9018958f04581910df48
Adil Bel Mqadem <abel-mqa@e1r1p9.1337.ma>
2024-11-23 13:41:15 +0100

    ex02 setup

--- ex02/Array.cpp
+++ ex02/Array.cpp
@@ -1,5 +1,2 @@
 #include "Array.hpp"
 
-int main() {
-
-}
--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -1,6 +1,20 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <iostream>
 
+template <typename T> class Array {
+    private:
+        T *array;
+        unsigned int size;
+    public:
+        Array();
+        Array(unsigned int n);
+        Array(Array const &);
+        Array &operator=(Array const &);
+        ~Array();
+        T &operator[](unsigned int i);
+        unsigned int getSize() const;
+};
 
 #endif
